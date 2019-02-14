d7123e97ce5860c2e045713d669e8d926a906a4f
kevinluu <this.kevinluu@gmail.com>
2019-02-14 15:16:14 -0500

    added mongoose and models

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
+// import db models
+const User = require('./models/Users');
+const Post = require('./models/Post');
+
+// connect to mongo db
 mongoose
   .connect(process.env.MONGO_URI, { userNewUrlParser: true })
-  .then(() => console.log('DB connected'))
+  .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
+// define type definition
 const typeDefs = gql`
   type Todo {
     task: String
@@ -18,14 +25,20 @@ const typeDefs = gql`
   }
 `;
 
+// define the resolvers
 const resolvers = {
   Query: {
     getTodos: () => todos
   }
 };
 
+// setup apollo server
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
