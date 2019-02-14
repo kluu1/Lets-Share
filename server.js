2718e64811df141d31f0c7d0b9bb51144d559d6a
kevinluu <this.kevinluu@gmail.com>
2019-02-14 12:04:54 -0500

    setup apollo-server, typeDefs, resolvers

---
+++ server.js
@@ -0,0 +1,44 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const todos = [
+  { task: 'Wash car', completed: false },
+  { task: 'Wash window', completed: false },
+  { task: 'Wash clothes', completed: false }
+];
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+
+  type Query {
+    getTodos: [Todo]
+  }
+
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is listening on ${url}`);
+});
