46e1c4d2b30a43042b625c72684a6d3017a9a55f
kevinluu <this.kevinluu@gmail.com>
2019-02-15 09:08:09 -0500

    added getPost query

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,35 @@
 module.exports = {
   Query: {
-    getUser: () => null
+    getPosts: async (_, args, { Post }) => {
+      const posts = await Post.find({})
+        .sort({ createdDate: 'desc' })
+        .populate({
+          path: 'createdBy',
+          model: 'User'
+        });
+      return posts;
+    }
   },
+
   Mutation: {
-    // destructure username, email, password from 'args' and destructure 'User' from context
+    addPost: async (
+      _,
+      // destructure fields from 'args'
+      { title, imageUrl, categories, description, creatorId },
+      // destructure 'Post' from context
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        imageUrl,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
+    },
+
+    // destructure fields from 'args' and destructure 'User' from context
     signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
       if (user) {
