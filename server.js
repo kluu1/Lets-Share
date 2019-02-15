69af5fa6740e03e90ac30418b3169d40a054f304
kevinluu <this.kevinluu@gmail.com>
2019-02-15 13:42:58 -0500

    cleaned up vue boilerplate for project

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// import environment variables and mongoose models
 require('dotenv').config();
-
-// import db models
 const User = require('./models/Users');
 const Post = require('./models/Post');
 
+// import graphql typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
 
-// connect to mongo db
+// connect to mongodb
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
-// setup apollo server
+// create apollo/graphql server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -29,6 +29,7 @@ const server = new ApolloServer({
   }
 });
 
+// start apollo/graphql server
 server.listen().then(({ url }) => {
   console.log(`Server is listening on ${url}`);
 });
